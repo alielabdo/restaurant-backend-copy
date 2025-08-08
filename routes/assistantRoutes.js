edb366da31958bfa3ba1d5f68d954d504d99e512
alielabdo <alloyali48@gmail.com>
2025-08-08 14:12:20 +0300

    syntax fixed

--- routes/assistantRoutes.js
+++ routes/assistantRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { spawn } = require("child_process");
-const Ingredient = require("../models/ingredientModel"); // Or your exact model path
+const Ingredient = require("../models/Ingredient"); // Or your exact model path
 const fs = require("fs");
 const multer = require("multer");
 const path = require("path");
